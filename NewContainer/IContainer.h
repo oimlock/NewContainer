9d34911ee8fcffae44d48757be012d62e6e23710
oimlock <81167991+oimlock@users.noreply.github.com>
2021-04-09 20:07:26 +0200

    Implemet interface class (#1)
    * Implemet interface class
    
    * Remove filters

---
+++ NewContainer/IContainer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+using std::vector;
+
+class IContainer {
+public:
+	virtual int get(int i) = 0;
+	virtual void insert(int i, int x) = 0;
+	virtual void erase(int i) = 0;
+	virtual void set(int i, int x) = 0;
+	virtual int size() = 0;
+	virtual bool empty() = 0;
+	virtual void clear() = 0;
+	virtual int summ_range(int left, int right) = 0;
+};
--- NewContainer/NewContainer.h
+++ NewContainer/NewContainer.h
@@ -1,95 +1,49 @@
 #pragma once
 
+#include <vector>
+#include "IContainer.h"
+
 using std::vector;
 
-class NewContainer {
+class NewContainer: public IContainer {
 public:
 
-	int operator[]  (int i) const {
+	int get(int i) override {
 		return container_[i];
 	}
 
-	int& operator[]  (int i) {
-		return container_[i];
-	}
-
-	void insert(int i, int x) {
+	void insert(int i, int x) override {
 		container_.insert(container_.begin() + i, x);
-		k_ = sqrt(container_.size());
-		container_sqrt_.resize((container_.size() + k_ - 1) / k_);
-		rebalance();
 	}
 
-	void erase(int i) {
+	void erase(int i) override {
 		container_.erase(container_.begin() + i);
 	}
 
-	int size() {
+	void set(int i, int x) override {
+		container_[i] = x;
+	}
+
+	int size() override {
 		return container_.size();
 	}
 
-	int empty() {
+	bool empty() override {
 		return !container_.empty();
 	}
 
-	void clear() {
+	void clear() override {
 		container_.clear();
 	}
 
-	int summ_range(int left, int right) {
-		section_.left = left;
-		section_.right = right;
-		if (section_.left / k_ == section_.right / k_) {
-			return summ_in_one_sections();
-		}
-		else {
-			return summ_with_diff_sections();
-		}
-	}
-
-private:
-
-	struct Range {
-		int left;
-		int right;
-	};
-
-	int k_;
-	Range section_;
-	vector <int> container_;
-	vector <int> container_sqrt_;
-
-	int summ_in_one_sections() {
+	int summ_range(int left, int right) override {
 		int summ = 0;
-		for (int i = section_.left; i < section_.right + 1; ++i) {
+		for (int i = left; i <= right; ++i) {
 			summ += container_[i];
 		}
 		return summ;
 	}
 
-	int summ_with_diff_sections() {
-		Range d;
-		d.left = section_.left / k_ + 1;
-		d.right = section_.right / k_ - 1;
-		int summ = 0;
-
-		for (int i = d.left; i <= d.right; ++i) { // основное тело
-			summ += container_sqrt_[i];
-		}
-		for (int j = section_.left; j < d.left * k_; ++j) { // левый хвост
-			summ += container_[j];
-		}
-		for (int j = (d.right + 1) * k_; j <= section_.right; ++j) { // правый хвост
-			summ += container_[j];
-		}
-		return summ;
-	}
-	void rebalance() {
-		container_sqrt_.clear();
-		k_ = sqrt(container_.size());
-		container_sqrt_.resize((container_.size() + k_ - 1) / k_);
-		for (int i = 0; i < container_.size(); ++i) {
-			container_sqrt_[i / k_] += container_[i];
-		}
-	}
+private:
+	vector <int> container_;
 };
